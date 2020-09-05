92351e8aa5267569dda1674b28e3fb7cd2368f9f
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-09-05 19:46:28 +0100

    UI update
    Implemented submit form on index page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//React and Navigation
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+//Components
+import Home from "./components/home.component";
+import ShowPokemon from "./components/pokemon.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return(
+			<Router>
+				<div className="container">
+					<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+						<Link to="/" className="navbar-brand">Shakespearemon</Link>	
+					</nav>
+					<br/>
+					<Route path="/" exact component={Home}/>
+					<Route path="/pokemon/:name" component={ShowPokemon}/>
+				</div>
+			</Router>
+		);
+	}
 }
-
 export default App;
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 export default class Home extends Component {
+	constructor(props){
+		super(props);
+		this.onChangePokemonName = this.onChangePokemonName.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+		this.state = {
+			entered_pokemon: "",
+		}
+	}
+	onChangePokemonName(event) {
+		this.setState({
+			entered_pokemon: event.target.value
+		});
+	}
+	onSubmit(event){
+		event.preventDefault(); //back-end isn't ready yet, so the app can't handle the submit behaviour properly yet.
+		console.log(`Form submitted:`);
+		console.log(`Pokemon requested: ${this.state.entered_pokemon}`);
+		this.setState({
+			entered_pokemon: ""
+		})
+	}
     render() {
         return (
             <div>
-                <p>Valorous morrow to thee, Trainer! Welcome to the realm of pokémon! Mine name is Shakespeare! 
+                <p>Valorous morrow to thee, Trainer! Welcome to the realm of Pokémon! Mine name is Shakespeare! 
 				People calleth me the Pokémon bard! This realm is did inhabit by creatures did doth clepe Pokémon! 
 				For some people, Pokémon art pets. Others useth them for fights. Myself...I study Pokémon as a profession.</p>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Pokémon Name:</label>
+						<input type="text" 
+							className="form-control" 
+							value={this.state.entered_pokemon} 
+							onChange={this.onChangePokemonName}/>
+					</div>
+					<div>
+						<input type="submit" value="GO!" className="btn btn-success"/>
+					</div>
+				</form>
             </div>
         )
     }
